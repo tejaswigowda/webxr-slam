b83c6aeb90c87bb95e06bed65aa2f44f85bdccea
Tejaswi Gowda <tejaswi@asu.edu>
2024-06-10 17:57:28 -0700

    ac

---
+++ ws-test.js
@@ -0,0 +1,17 @@
+var port = process.argv[2] || 8888;
+
+var WebSocket = require('ws');
+
+var ws = new WebSocket('ws://localhost:' + port + '/xr-slam-client');
+
+ws.on('open', function open() {
+    console.log('connected');
+});
+
+ws.on('close', function close() {
+    console.log('disconnected');
+});
+
+ws.on('message', function incoming(data) {
+    console.log('received: %s', data);
+});
